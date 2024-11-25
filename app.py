54bfd3631f7d1e08856d4ef2a42e9107ced82f1c
madmattp <mpqfreitas@gmail.com>
2024-11-25 17:17:52 -0300

    Added parameters to html

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    max_threads = os.cpu_count()  # Limite de threads baseado no número de núcleos
+
     if request.method == 'POST':
-        text = request.form.get('text_input')
-        checkbox = request.form.get('checkbox')
+        # Recebe o arquivo
+        file = request.files['input']
 
-        file = request.files['file_input']
-
+        # Recebe os demais parâmetros
+        matrix = request.form['matrix']
+        filter_type = request.form['filter']
+        sharpness = float(request.form['sharpness'])
+        contrast = float(request.form['contrast'])
+        downscale = int(request.form['downscale'])
+        threads = int(request.form['threads'])
+        
+        if file:
+            pass
         
         return redirect(url_for('success'))
 
-    return render_template('index.html')
+    return render_template('index.html', max_threads=max_threads)
 
 @app.route('/success')
 def success():
